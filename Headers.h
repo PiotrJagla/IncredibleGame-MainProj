fa0131d257ab3590e3660f7a9014851d4378f7bb
PiotrJagielny <piotrek2002.09@gmail.com>
2022-04-12 20:41:14 +0200

    Incredible Game

---
+++ Headers.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Audio.hpp"
+#include "SFML/Network.hpp"
+
+#include <iostream>
+
+
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "Headers.h"
+
+
+int main()
+{
+
+	return 0;
+}
